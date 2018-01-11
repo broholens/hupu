953c762284fccba8f9f65e315267f3fbbf0203ab
broholens <broholens@github.com>
2018-01-12 02:16:51 +0800

    headless

--- hupu.py
+++ hupu.py
@@ -17,35 +17,25 @@ class HuPu:
     提供登录、评论功能
     需要人为滑动验证登录
     """
-
+    # 每人每天最多回复自己评论数
     max_comment_count = 300
-    logger = logging.getLogger(__name__)
-    driver = webdriver.Chrome()
-    driver.set_page_load_timeout(10)
-    comment_count = 0
-    post_ids = []
-    # def __init__(self):
-    #     self.logger = logging.getLogger(__name__)
-    #     self.driver = webdriver.Firefox()
-    #     self.driver.set_page_load_timeout(10)
-    #     self.comment_count = 0
-    #     self.post_ids = []
 
-    def login(self, username, password):
-        """
-        人为滑动验证，时间为１０秒
-        """
-        if self.request('https://passport.hupu.com/pc/login') == 110:
-            return
-        try:
-            self.driver.find_element_by_id('J_username').send_keys(username)
-            self.driver.find_element_by_id('J_pwd').send_keys(password)
-            time.sleep(10)
-            self.driver.find_element_by_id('J_submit').click()
-            time.sleep(1)
-        except:
-            self.logger.error('login error!')
-            self.driver.close()
+    logger = logging.getLogger(__name__)
+
+    # headless chrome
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(chrome_options=options)
+
+    driver.set_page_load_timeout(10)
+
+    # 本日回帖数量
+    comment_count = 0
+
+    # 帖子id
+    post_ids = set()
 
     def wx_login(self):
         """
@@ -105,9 +95,6 @@ class HuPu:
                 post.get_attribute('href')
                 for post in self.driver.find_elements_by_xpath(xp)[:30]
             ]
-            # for post in self.driver.find_elements_by_xpath(xp)[2:30]:
-            #     print(post.get_attribute('href'))
-            #     self.comment(post.get_attribute('href'), commentary)
         except:
             posts = []
             self.logger.error('comment latest 20 error!')
@@ -119,13 +106,13 @@ class HuPu:
                 self.logger.info('comment %s successfully!', post)
             time.sleep(120)
 
-
-if __name__ == '__main__':
-    hupu = HuPu()
-    hupu.login('13148365571', 'i616463')
-    commentary = '一切均为实拍 购买其他鞋子 降价信息 细节照片都可以咨询微信 clpro7 无限回收闲置球鞋 急用钱卖鞋的 寻求多方合作~'
-    while True:
-        hupu.comment_latest_20_posts(commentary)
-        if hupu.comment_count > hupu.max_comment_count:
-            break
-    hupu.driver.close()
+#
+# if __name__ == '__main__':
+#     hupu = HuPu()
+#     hupu.wx_login()
+#     commentary = '一切均为实拍 购买其他鞋子 降价信息 细节照片都可以咨询微信 clpro7 无限回收闲置球鞋 急用钱卖鞋的 寻求多方合作~'
+#     while True:
+#         hupu.comment_latest_20_posts(commentary)
+#         if hupu.comment_count > hupu.max_comment_count:
+#             break
+#     hupu.driver.close()
