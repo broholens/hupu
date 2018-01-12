8d46822616b7a7bdf6092f84a4d84cdb28c5a9ba
broholens <broholens@github.com>
2018-01-12 19:16:20 +0800

    base64图片

--- hupu_app.py
+++ hupu_app.py
@@ -1,8 +1,7 @@
 import re
-
 import web
-
 from hupu import HuPu
+from settings import TABLE
 
 urls = (
     '/login', 'login',
@@ -12,37 +11,50 @@ urls = (
 
 app = web.application(urls, globals())
 
-hupu = HuPu()
+hp = HuPu()
 
 
 class login:
 
     def GET(self):
-        qrcode_link = hupu.wx_login()
-        if not qrcode_link:
-            return 'qrcode link unreachable'
-        raise web.seeother(qrcode_link)
+        base64_img = hp.wx_login()
+        # if not os.path.exists('qrcode.png'):
+        #     return 'qrcode unreachable'
+        return """
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>wechat login</title>
+            </head>
+            <body>
+                <div style="text-align: center">
+                    <img src="data:image/png;base64,{}" style="margin: 0 auto; display: block;">
+                </div>
+            </body>
+            </html>        
+        """.format(base64_img)
+        # raise web.seeother(qrcode_link)
 
 
 class insert:
     def GET(self, posts):
         for post_id in re.findall('\d+', posts):
-            hupu.post_ids.add(post_id)
+            TABLE.update_one(
+                {'post_id': post_id},
+                {'$set': {'post_id': post_id}},
+                upsert=True
+            )
         return {
-            number: post_id
-            for number, post_id in enumerate(hupu.post_ids)
+            number: post_id.get('post_id')
+            for number, post_id in enumerate(TABLE.find())
         }
 
 
 class delete:
     def GET(self, posts):
         for post_id in re.findall('\d+', posts):
-            hupu.post_ids.remove(post_id)
+            TABLE.delete_one({'post_id': post_id})
         return {
-            number: post_id
-            for number, post_id in enumerate(hupu.post_ids)
+            number: post_id.get('post_id')
+            for number, post_id in enumerate(TABLE.find())
         }
-
-
-if __name__ == '__main__':
-    app.run()
