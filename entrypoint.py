18a711437fd8fd0185d5d9edc14f9469b2f6b5a8
broholens <broholens@github.com>
2018-01-12 19:14:27 +0800

    添加入口函数

---
+++ entrypoint.py
@@ -0,0 +1,13 @@
+from multiprocessing import Process
+from hupu_app import app, hp
+
+
+def main():
+    p1 = Process(target=app.run)
+    p2 = Process(target=hp.run)
+    p1.start()
+    p2.start()
+
+
+if __name__ == '__main__':
+    main()
