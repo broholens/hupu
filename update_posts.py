3e6f604a7823c5b59cfa8bb25137717b31c01891
broholens <broholens@github.com>
2018-03-09 09:24:28 +0800

    use apscheduler

--- update_posts.py
+++ update_posts.py
@@ -1,16 +1,21 @@
 from multiprocessing import Process
-from hupu_app import app, hp
+from apscheduler.schedulers.background import BackgroundScheduler
+from hupu_app import app, hupu
 
 
-# def main():
-#     p1 = Process(target=app.run)
-#     p2 = Process(target=hp.run)
-#     p1.start()
-#     p2.start()
+def schedule():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(hupu.store_posts, 'interval', hours=2)
+    scheduler.start()
+
+
+def main():
+    p1 = Process(target=app.comment_posts, kwargs={'port': 80})
+    p2 = Process(target=schedule)
+    p1.start()
+    hupu.is_logged()
+    p2.start()
 
 
 if __name__ == '__main__':
-    # main()
-    hupu = HuPu()
-    hupu.login('wechat')
-    hupu.get_latest_post()
+    main()
