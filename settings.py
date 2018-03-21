4dc5350ac6a677c77f5a4c4fb12bbd4a741c9cb9
broholens <broholens@github.com>
2018-03-21 17:10:15 +0800

    send email when error occurs

--- settings.py
+++ settings.py
@@ -1,31 +1,64 @@
 import logging
+import requests
 from pymongo import MongoClient
+
 
 client = MongoClient()
 DB = client.hupu
 
+
+class MongoFormatter(logging.Formatter):
+
+    def format(self, record):
+        """Formats LogRecord into python dictionary."""
+        return {
+            'timestamp': record.created,
+            'level': record.levelname,
+            'message': record.getMessage(),
+        }
+
+
+class MongoHandler(logging.Handler):
+
+    def __init__(self, level=logging.NOTSET):
+        logging.Handler.__init__(self, level)
+        self.formatter = MongoFormatter()
+        self.collection = DB.logs
+
+    def emit(self, record):
+        """Inserting new logging record to mongo database."""
+        try:
+            self.collection.save(self.format(record))
+        except Exception:
+            pass
+
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s %(levelname)s %(message)s',
-    datefmt='%a, %d %b %Y %H:%M:%S',
-    filename='hupu.log',
-    filemode='a'
+    datefmt='%d %b %Y %H:%M:%S',
 )
 
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s %(levelname)-6s %(message)s')
-console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
+# add mongohandler
+logger = logging.getLogger('')
+logger.setLevel(logging.INFO)
+logger.addHandler(MongoHandler())
+
+
+def send_email(msg, subject='hupu'):
+    requests.post(
+        'http://0.0.0.0:5000/email/',
+        params={'subject': subject, 'msg': msg}
+    )
 
 # pymysql.err.OperationalError: (2003, "Can't connect to MySQL server on 'host'
 # ([Errno -8] Servname not supported for ai_socktype)")
 
-mysql_config = dict(
-    host='localhost',
-    user='root',
-    passwd='      ',
-    db='hupu',
-    charset='utf8',
-    unix_socket='/var/run/mysqld/mysqld.sock'
-)
+# mysql_config = dict(
+#     host='localhost',
+#     user='root',
+#     passwd='      ',
+#     db='hupu',
+#     charset='utf8',
+#     unix_socket='/var/run/mysqld/mysqld.sock'
+# )
